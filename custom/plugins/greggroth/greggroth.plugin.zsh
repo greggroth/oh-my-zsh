c0b04885cd0ea105672f7f615d367b5107646fb5
Greggory Rothmeier <greggroth@gmail.com>
2012-01-21 09:51:33 -0500

    added my custom plugin

---
+++ custom/plugins/greggroth/greggroth.plugin.zsh
@@ -0,0 +1,42 @@
+# Shortcut to my code projects
+c() { cd ~/Programing/$1; }
+_c() { _files -W ~/Programing -/; }
+compdef _c c
+
+# Shortcut to my papers directory
+p() { cd ~/Dropbox/Documents/Papers/$1; }
+_p() { _files -W ~/Dropbox/Documents/Papers -/; }
+compdef _p p
+
+# Shortcut for opening finder
+alias f='open .'
+
+# Some extra things for git
+alias ggraph='git log --date-order --graph --date=short --pretty="%h (%ad): %s"'
+compdef _git ggraph=git-log
+gcam() { git commit -a -m $1; }
+compdef _git gcam=git-commit
+
+# Build pdflatex and bibtex
+pbpp() {
+  if [ -z $1 ]; then
+    echo "No arugment given"
+    return
+  fi
+  if [ ! -e "${1}.tex" ]; then
+    echo "Tex file not found.  Verify that the current directory contains a file named ${1}.tex"
+    return
+  fi
+  pdflatex $1
+  bibtex $1
+  pdflatex $1
+  pdflatex $1
+  open "${1}.pdf"
+}
+
+lbll() {
+  latex $1
+  bibtex $1
+  latex $1
+  latex $1
+}
