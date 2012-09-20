44787816d9ba28b85150ebbc7923a484f66a970a
Greggory Rothmeier <gregg@highgroove.com>
2012-09-20 14:31:08 -0500

    tmux shortcuts and better git graph

--- custom/plugins/greggroth/greggroth.plugin.zsh
+++ custom/plugins/greggroth/greggroth.plugin.zsh
@@ -10,8 +10,14 @@ alias f='open .'
 alias v='vim'
 alias vi='vim'
 
+# Tmux Aliases
+alias ta='tmux attach-session -t'
+alias tlist='tmux list-sessions'
+alias tnew='tmux new-sesion -s'
+alias tkill='tmux kill-sessions -t'
+
 # Some extra things for git
-alias ggraph='git log --date-order --graph --date=short --pretty="%h (%ad): %s (%an)"'
+alias ggraph='git log --date-order --graph --date=short --pretty="%Cred%h%Creset (%Cgreen%cr%Creset): %s (%Cblue%an%Creset)" -10 | column -tx -s \:'
 compdef _git ggraph=git-log
 gcam() { git commit -a -m $1; }
 compdef _git gcam=git-commit
@@ -30,3 +36,6 @@ alias mount_home='mount_smbfs //Greggory@greggory-pc/Users/Greggory/ ~/share'
 
 # Open a file with VLC
 vlc() { open $1 -a vlc; }
+
+# Syntax-highlighted cat
+ccat() { source-highlight -i $1 -o STDOUT -f esc256; }
